afa71f7e31b5a334b8f60e7e82ee30808baf30c8
Spud <jeffrypig23@gmail.com>
2022-03-10 15:42:03 -0800

    Get blog info

--- get_number_of_posts_util.cpp
+++ get_number_of_posts_util.cpp
@@ -5,8 +5,8 @@
 #include <spdlog/sinks/basic_file_sink.h>
 #include "src/FoxWebhook.hpp"
 #include <iostream>
-#include <npf/content/image.hpp>
 #include <cpr/cpr.h>
+#include <npf/content/image.hpp>
 
 // Add sleep function based on OS
 #ifdef _WIN32
@@ -18,9 +18,28 @@
 #else
 
 #include <unistd.h> // UNIX sleep
-#include <iostream>
 
 #endif
+
+unsigned long getInput() {
+
+	// Get the number of posts to get for each foxWebhook.
+	unsigned long input = 0;
+	std::cout << "How many posts should be retrieved? "; // << std::endl; - no new lines.
+	std::cin >> input;
+
+	// Validate user input.
+	while (std::cin.fail()) {
+
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cout << "Invalid input." << std::endl;
+		std::cout << "Retry: "; // << std::endl; - no new lines.
+		std::cin >> input;
+	}
+
+	return input;
+}
 
 int main() {
 
@@ -38,7 +57,7 @@ int main() {
 
 	// Check the status of the load.
 	if (loadStatus != 0) {
-		logger->error(fmt::format("Unable to load FoxWebooks: {}", loadStatus));
+		logger->error(fmt::format("Unable to load FoxWebhooks: {}", loadStatus));
 		return loadStatus;
 	}
 	logger->info(fmt::format("Loaded {} FoxWebhooks(s) successfully", foxWebhooks.size()));
@@ -46,33 +65,48 @@ int main() {
 	// Iterate though each foxwebhook.
 	for (FoxWebhook &foxWebhook : foxWebhooks) {
 
-		// Get the number of posts to get for each foxWebhook.
-		unsigned long input = 0;
-		std::cout << "How many posts should be retrieved? "; // << std::endl; - no new lines.
-		std::cin >> input;
+		// Get the blog json from the tumblr api.
+		cpr::Response blogJson = foxWebhook.tumblrApi.getBlogInfoJson(foxWebhook.blog);
 
-		// Validate user input.
-		while (std::cin.fail()) {
+		// Verify that the response for retrieving the blog json was successful (will return with 200).
+		if (blogJson.status_code != 200) {
 
-			std::cin.clear();
-			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-			std::cout << "Invalid input." << std::endl;
-			std::cout << "Retry: "; // << std::endl; - no new lines.
-			std::cin >> input;
+			// Log that we could not get the blog info and return early.
+			// DO NOT SET THE PREVIOUS POST (that way this will be rerun).
+			logger->warn("Unable to retrieve blog info!\nResponse code {0}.\n{1}", blogJson.status_code, blogJson.text);
+			return ErrorCodes::INVALID_JSON_FORMAT;
 		}
+		logger->info(fmt::format("Blog response json vvv\n{}\n", blogJson.text));
+
+		// Create an actual blog object.
+		Blog blog = Blog::generateBlog(blogJson.text.c_str());
+
+		// Get the number of posts to retrieve.
+		unsigned long input = getInput();
 		logger->info(fmt::format("Retrieving {} posts...", input));
 
-		// Fetch the json data from the tumblr api.
-		cpr::Response json = foxWebhook.tumblrApi.getPostsJson(foxWebhook.blog, input);
+		// Fetch the posts json data from the tumblr api.
+		cpr::Response postsJson = foxWebhook.tumblrApi.getPostsJson(foxWebhook.blog, input);
 
 		// Check the response ode for the post. If it isn't 200 be sure to log as an error and return now.
-		if (json.status_code != 200) {
+		if (postsJson.status_code != 200) {
 
-			logger->error("Unable to get initial post for blog!\nResponse code {0}.\n{1}", json.status_code, json.text);
-			return ErrorCodes::CANNOT_GET_INITIAL_POST;
+			logger->error("Unable to get initial post for blog!\nResponse code {0}.\n{1}", postsJson.status_code, postsJson.text);
+			return ErrorCodes::INVALID_JSON_FORMAT;
 		}
-		logger->info(fmt::format("Response json vvv\n{}\n", json.text));
+		logger->info(fmt::format("Posts response json vvv\n{}\n", postsJson.text));
 
+		// Generate posts from the response postsJson.
+		std::vector<std::shared_ptr<Post>> posts = Post::generatePosts(postsJson.text.c_str());
+
+		std::reverse(posts.begin(), posts.end());
+		for (std::shared_ptr<Post> post : posts) {
+
+			// TODO post to discord.
+
+			// Sleep for 5 seconds.
+			sleep(5);
+		}
 	}
 
 	logger->info("Exiting...");
